758fb974e02a799f10fcb3ed05cc5243b9f3595c
Alexandra Charland <alch5406@colorado.edu>
2021-10-15 21:20:26 -0600

    basic movement animations

--- VampireHunter/Assets/PlayerMovement.cs
+++ VampireHunter/Assets/PlayerMovement.cs
@@ -5,15 +5,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D playerrb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float xDir = 0f;
+
+    [SerializeField] private float moveSpeed = 5f;
+    
+    private enum MovementState{idle,running,jumping,falling}
+
+    private MovementState state = MovementState.idle;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Debug.Log("Start");
+        playerrb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        xDir = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerrb.velocity = new Vector2(playerrb.velocity.x,moveSpeed);
+        }
+        playerrb.velocity = new Vector2(xDir * moveSpeed,playerrb.velocity.y);
+        AnimationChange();
+    }
+
+    private void AnimationChange()
+    {
+        MovementState state;
+        if (xDir > 0f)
+        {
+            //anim.SetBool("Running",true);
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if(xDir < 0f)
+        {
+            //anim.SetBool("Running", true);
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else
+        {
+            //anim.SetBool("Running",false);
+            state = MovementState.idle;
+        }
+
+        if (playerrb.velocity.y > 0.1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (playerrb.velocity.y < -0.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("State",(int)state);
     }
 }
